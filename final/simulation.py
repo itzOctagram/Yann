fb753f8e51c668f3c4b27f075074bca5b0f55b6f
karan8404 <kkawathia8404@gmail.com>
2024-09-06 15:53:44 +0530

    refactor: Update Vehicle speed to handle different speeds for acceleration and deceleration

--- final/simulation.py
+++ final/simulation.py
@@ -7,7 +7,7 @@ class Vehicle:
     direction: Literal['up', 'down', 'left', 'right']
     # left - sharp turn, right - moderate turn
     turnDirection: Literal['left', 'right', 'straight']
-    speed: int
+    speed: Tuple[int, int]
     location: Tuple[int, int]
     image: pygame.Surface
 
@@ -16,7 +16,7 @@ class Vehicle:
         self.direction = direction
         self.turnDirection = turnDirection
         self.speed = self.getMaxSpeed()
-        self.location = self.getStartLocation()
+        self.setStartLocation()
         self.image = pygame.image.load(f'./images/{direction}/{type}.png')
 
     def getMaxSpeed(self):
@@ -24,25 +24,47 @@ class Vehicle:
         return maxSpeed[self.type]
 
     def getAcceleration(self):
-        acceleration = {'car': 1, 'bus': 0.6, 'bike': 1.4}
+        acceleration: dict[str, int] = {'car': 1, 'bus': 0.6, 'bike': 1.4}
         return acceleration[self.type]
 
-    def getStartLocation(self):
-        startLocation = {'up': (640, 720), 'down': (
-            640, 0), 'left': (1250, 360), 'right': (0, 360)}
-        return startLocation[self.direction]
+    def setStartLocation(self):
+        screen_info = (1280, 720)
+        startLocation = {'up': (screen_info[0]//2, screen_info[1]), 'down': (screen_info[0]//2, 0),
+                         'left': (screen_info[0], screen_info[1]//2), 'right': (0, screen_info[1]//2)}
+        self.setLocation(startLocation[self.direction])
+
+    def setLocation(self, location: Tuple[int, int]):
+        size = self.getSize()
+        center = (size[0]//2, size[1]//2)
+        self.location = (location[0]-center[0], location[1]-center[1])
+        # self.location = location
 
     def getSize(self):
         size = {'car': (22, 54), 'bus': (26, 76), 'bike': (17, 38)}
+        reverse_size = {'car': (54, 22), 'bus': (76, 26), 'bike': (38, 17)}
         if (self.direction == 'up' or self.direction == 'down'):
             return size[self.type]
         else:
-            return size[self.type][::-1]
+            return reverse_size[self.type]
+        
+    def move(self, dt: float):
+        speed = self.speed
+        acceleration = self.getAcceleration()
+        if self.direction == 'up':
+            self.location = (self.location[0], self.location[1] - speed*dt)
+        elif self.direction == 'down':
+            self.location = (self.location[0], self.location[1] + speed*dt)
+        elif self.direction == 'left':
+            self.location = (self.location[0] - speed*dt, self.location[1])
+        elif self.direction == 'right':
+            self.location = (self.location[0] + speed*dt, self.location[1])
 
+
+vehicles = [Vehicle('car', 'up'), Vehicle('car', 'down'),
+            Vehicle('car', 'left'), Vehicle('car', 'right')]
 
 if __name__ == "__main__":
     pygame.init()
-    screen_info = pygame.display.Info()
     screen = pygame.display.set_mode((1280, 720))
     clock = pygame.time.Clock()
     running = True
@@ -51,7 +73,6 @@ if __name__ == "__main__":
     # Load the background image
     background_image = pygame.image.load('./images/mod_int3.png')
 
-    vehicle = Vehicle('car', 'left')
     while running:
 
         for event in pygame.event.get():
@@ -68,7 +89,8 @@ if __name__ == "__main__":
         # Draw the scaled background image
         screen.blit(scaled_background, (0, 0))
 
-        screen.blit(vehicle.image, vehicle.location)
+        for vehicle in vehicles:
+            screen.blit(vehicle.image, vehicle.location)
 
         pygame.display.flip()
         dt = clock.tick(60)/1000
